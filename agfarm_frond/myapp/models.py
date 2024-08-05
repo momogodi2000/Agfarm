845947d85b46844f1736db424f3e63010842412f
Percy237 <percylarson17@gmail.com>
2024-08-05 12:19:07 +0100

    Update Django settings for CORS

--- agfarm_frond/myapp/models.py
+++ agfarm_frond/myapp/models.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
     ROLE_CHOICES = [
-        ('admin', 'Admin'),
-        ('financial_institution', 'Financial Institution'),
-        ('government_agency', 'Government Agency'),
-        ('logistics_provider', 'Logistics Provider'),
+        ("admin", "Admin"),
+        ("financial_institution", "Financial Institution"),
+        ("farmer", "Farmer"),
+        ("government_agency", "Government Agency"),
+        ("logistics_provider", "Logistics Provider"),
     ]
     role = models.CharField(max_length=30, choices=ROLE_CHOICES)
     phone_number = models.CharField(max_length=12, unique=True, null=True, blank=True)
